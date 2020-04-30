df2d7e01621e5a24613da80896d932cd7a37bb5d
Justin Sheldon <JSheldon3488@gmail.com>
2020-04-30 00:35:10 -0500

    Added Dueling Network

--- neural_network.py
+++ neural_network.py
@@ -27,3 +27,41 @@ class QNetwork(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         return self.fc4(x)
+
+
+class Dueling_QNetwork(nn.Module):
+    """ Dueling Neural Network architecture with state value and action advantage evaluation """
+
+    def __init__(self, state_size, action_size, seed):
+        """ Initialize parameters and set up layers of the model
+
+        :param state_size: Dimension of state space
+        :param action_size: Dimension of action space
+        :param seed: Random seed
+        """
+        super(Dueling_QNetwork, self).__init__()
+        self.seed = torch.manager_seed(seed)
+        # Shared Layers
+        self.fc1 = nn.Linear(state_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+
+        # Individual Layers
+        self.fc_value = nn.Linear(64,32)
+        self.fc_action = nn.Linear(64,32)
+        self.state_value = nn.Linear(32,1)
+        self.action_advantage = nn.Linear(32,action_size)
+
+    def forward(self, state):
+        """ Dueling network that maps state to action values using a Dueling network architecture"""
+        # Shared Layers
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+
+        # Individual Layers
+        state_value = F.relu(self.fc_value(x))
+        action_advantage = F.relu(self.fc_action(x))
+        state_value = self.state_value(state_value)
+        action_advantage = self.action_advantage(action_advantage)
+
+        # Combine for final value using equation (9) from paper
+        return state_value + (action_advantage - action_advantage.mean())
